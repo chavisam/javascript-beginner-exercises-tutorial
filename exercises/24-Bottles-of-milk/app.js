baa41a4b8f23b7a8361f4f0b702b2a0ab762fe2f
Javier <jmanteca@jmanvel.com>
2021-09-19 09:04:02 +0000

    todo ok

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -4,7 +4,14 @@ for(let i =99 ; i>-1 ; i--){
     if(i==1){
         song =  i + ' bottle of milk on the wall, ' 
         + i + ' bottle of milk.' + ' Take one down and pass it around, '
-         + ' no more bottles of milk on the wall.';
+         + 'no more bottles of milk on the wall.';
+        console.log(song);
+    }
+
+    if(i==2){
+        song =  i + ' bottles of milk on the wall, ' 
+        + i + ' bottles of milk.' + ' Take one down and pass it around, '
+        + (i-1) + ' bottle of milk on the wall.';
         console.log(song);
     }
 
@@ -14,7 +21,7 @@ for(let i =99 ; i>-1 ; i--){
         
     }
 
-    if(i>1){
+    if(i>2){
     song =  i + ' bottles of milk on the wall, ' 
     + i + ' bottles of milk.' + ' Take one down and pass it around, '
      + (i-1) + ' bottles of milk on the wall.';
